addc142fb839395196d4538d7960cb3bd272bd6b
Mark Wissink <markcwissink@gmail.com>
2019-02-09 16:07:38 -0500

    Adding mobile support

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -118,10 +118,57 @@ window.addEventListener('keydown', function(e) {
   if (e.keyCode == 38) { 			    //up
   	socket.emit('direction', 3);
   } else if (e.keyCode == 39) { 	//right
-	socket.emit('direction', 1);
+  	socket.emit('direction', 1);
   } else if (e.keyCode == 40) { 	//down
   	socket.emit('direction', 4);
   } else if (e.keyCode == 37){ 		//left
   	socket.emit('direction', 2);
   }
 });
+
+// https://stackoverflow.com/questions/2264072/detect-a-finger-swipe-through-javascript-on-the-iphone-and-android
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+};
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            socket.emit('direction', 2);
+        } else {
+            socket.emit('direction', 1);
+        }
+    } else {
+        if ( yDiff > 0 ) {
+            socket.emit('direction', 3);
+        } else {
+            socket.emit('direction', 4);
+        }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+};
