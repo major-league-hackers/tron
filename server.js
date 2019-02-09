1f18cbfbd4025e74f332135ef52f059f7f76e963
Mark Wissink <markcwissink@gmail.com>
2019-02-08 21:59:16 -0500

    Added basic server

--- server.js
+++ server.js
@@ -1,12 +1,45 @@
-const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const express  = require('express');
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-  res.send('Hello from App Engine!');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+// Socket server
+io.on('connection', socket => {
+	// Something
 });
+
+// game loop
+// https://github.com/timetocode/node-game-loop
+const hrtimeMs = function() {
+    let time = process.hrtime();
+    return time[0] * 1000 + time[1] / 1000000;
+}
+
+const TICK_RATE = 20;
+let tick = 0;
+let previous = hrtimeMs();
+let tickLengthMs = 1000 / TICK_RATE;
+
+const loop = () => {
+    setTimeout(loop, tickLengthMs)
+    let now = hrtimeMs();
+    let delta = (now - previous) / 1000;
+    // game.update(delta, tick) // game logic would go here
+    previous = now
+    tick++
+}
+
+loop(); // starts the loop
 
 // Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
 });
